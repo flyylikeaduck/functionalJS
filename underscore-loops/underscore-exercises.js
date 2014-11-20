452b7d9a61853d1c5e8bb375df202dd16adb8aff
thejenniekim <jk@udacity.com>
2014-11-19 20:49:18 -0800

    added notes

--- underscore-loops/underscore-exercises.js
+++ underscore-loops/underscore-exercises.js
@@ -35,7 +35,7 @@ var checkValue = function(array, val) {
   return result;
 };
 
-var testObj = {
+var testObj = { 
   one: 1,
   two: 2,
   three: 3
@@ -57,3 +57,26 @@ console.log(pusher(testObj));
 var map = function(object, func, context) {
   context = context || [];
 }
+
+
+
+  var createCallback = function(func, context, argCount) {
+    if (context === void 0) return func;
+    switch (argCount == null ? 3 : argCount) {
+      case 1: return function(value) {
+        return func.call(context, value);
+      };
+      case 2: return function(value, other) {
+        return func.call(context, value, other);
+      };
+      case 3: return function(value, index, collection) {
+        return func.call(context, value, index, collection);
+      };
+      case 4: return function(accumulator, value, index, collection) {
+        return func.call(context, accumulator, value, index, collection);
+      };
+    }
+    return function() {
+      return func.apply(context, arguments);
+    };
+  };
